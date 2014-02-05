5b4b5ec0f136c4b167a8baaf2ae53863d8e70bc0
Thomas J. Leeper <thosjleeper@gmail.com>
2014-02-05 16:37:48 +0100

    Allow function to handle stars

--- R/coefpaste.r
+++ R/coefpaste.r
@@ -9,31 +9,37 @@
 # val is a vector of values (e.g., regression coefficients)
 # par is a vector of parrenthetical values (e.g., standard errors)
 # digits is an optional two-item vector indicating the number of digits to which coefficients and errors should be rounded
+# stars is either a number specifying a threshold for adding a star to each output value (ratio of val to par) or a function taking two argumetns for generating a star (or stars)
 
-coefpaste <- function(val,par,digits=c(2,2)){
+coefpaste <- function(val,par,digits=c(2,2), stars=NULL){
 	if(length(digits)==1)
 		digits <- rep(digits,2)
 	# internal function
 	func <- function(coef,var){
 		# handle 'val'/'coef'
 		if(digits[1]>0)
-			coef <- sprintf(coef,fmt=paste("%#.",digits[1],"f",sep=""))
+			coefout <- sprintf(coef,fmt=paste("%#.",digits[1],"f",sep=""))
 		else if(digits[1]==0)
-			coef <- round(coef,0)
+			coefout <- round(coef,0)
 		else if(digits[1]<0){
 			cchar <- nchar(as.numeric(strsplit(as.character(coef),"[.]")[[1]][1])) + digits[1]
-			coef <- signif(coef,cchar)
+			coefout <- signif(coef,cchar)
 		}
 		# handle 'par'/'var'
 		if(digits[2]>0)
-			var <- sprintf(var,fmt=paste("%#.",digits[2],"f",sep=""))
+			varout <- sprintf(var,fmt=paste("%#.",digits[2],"f",sep=""))
 		else if(digits[2]==0)
-			var <- round(var,0)
+			varout <- round(var,0)
 		else if(digits[2]<0){
 			vchar <- nchar(as.numeric(strsplit(as.character(var),"[.]")[[1]][1])) + digits[2]
-			var <- signif(var,vchar)
+			varout <- signif(var,vchar)
 		}
-		output <- paste(coef," (",var,")",sep="")
+		if(is.null(stars))
+			output <- paste(coefout," (",varout,")",sep="")
+		else if(is.numeric(stars))
+			output <- paste(coefout,if (abs(coef/var)>stars) '*' else ''," (",varout,")", sep="")
+		else
+			output <- paste(coefout, stars(coef,var), " (",varout,")", sep="")
 		return(output)
 	}
 	# return
